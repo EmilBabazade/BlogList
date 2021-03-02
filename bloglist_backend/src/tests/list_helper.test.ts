0cb27f91532a1c08b459baeac71dc06ecb4d850a
emil <emilbabazade29@gmail.com>
2021-03-02 18:28:06 +0200

    some dummy tests

--- bloglist_backend/src/tests/list_helper.test.ts
+++ bloglist_backend/src/tests/list_helper.test.ts
@@ -6,3 +6,53 @@ test('dummy returns one', () => {
 	const result = listHelper.dummy(blogs)
 	expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+	const listWithOneBlog: listHelper.Blog[] = [
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		}
+	]
+  
+	test('when list has only one blog, equals the likes of that', () => {
+		const result = listHelper.totalLikes(listWithOneBlog)
+		expect(result).toBe(5)
+	})
+
+	const listWithManyBlogs: listHelper.Blog[] = [
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		},
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		},
+		{
+			_id: '5a422aa71b54a676234d17f8',
+			title: 'Go To Statement Considered Harmful',
+			author: 'Edsger W. Dijkstra',
+			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+			likes: 5,
+			__v: 0
+		}
+	]
+
+	test('when list has many blogs, equals the likes of that sum', () => {
+		const result = listHelper.totalLikes(listWithManyBlogs)
+		expect(result).toBe(15)
+	})
+})
--- bloglist_backend/src/utils/list_helper.ts
+++ bloglist_backend/src/utils/list_helper.ts
@@ -1,15 +1,22 @@
 interface Blog {
+	_id: string,
     title: string,
 	author: string,
 	url: string,
-	likes: number
+	likes: number,
+	__v: number
 }
 
 function dummy(blogs: Blog[]): number {
 	return 1
 }
 
+function totalLikes(blogs: Blog[]): number {
+	return blogs.map(b => b.likes).reduce((a, b) => a + b, 0)
+}
+
 export {
 	dummy,
-	Blog
+	Blog,
+	totalLikes
 }
