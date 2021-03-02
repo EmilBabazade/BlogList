cd9d1904839913e8cfaf93ba46dcd357374954ca
emil <emilbabazade29@gmail.com>
2021-03-02 19:11:37 +0200

    set up db integration testing

--- bloglist_backend/src/utils/config.ts
+++ bloglist_backend/src/utils/config.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const MONGODB_URI = process.env.MONGODB_URL
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+	? process.env.MONGODB_URL_TEST
+	: process.env.MONGODB_URL
 const PORT = process.env.PORT
 
 export {
